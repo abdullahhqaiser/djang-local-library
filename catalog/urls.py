58b87220ca98c7108cc1fac3dff689cde1fe2a32
abdullahhqaiser <abdullahh.qaiser@gmail.com>
2025-03-13 21:37:25 +0500

    add generic views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("books/", views.ListBookView.as_view(), name="books"),
+    path("books/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
+    # authors urls
+    path("authors/", views.ListAuthorView.as_view(), name="authors"),
+    path("authors/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, BookInstance, Genre, Author
+from django.views import generic
 
 
 def index(request):
@@ -24,3 +25,53 @@ def index(request):
     }
 
     return render(request, "index.html", context=context)
+
+
+class ListBookView(generic.ListView):
+    model = Book
+    """
+    context_object_name : used in template for accessing list of books,
+      which is added to context dict 
+    
+    """
+    context_object_name = "book_list"
+
+    """
+    template_name : points to html file,which is linked with this view.
+    """
+    template_name = "books/book_list.html"
+
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Book.objects.all()
+
+    # here context mean the data we pass to template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["verified"] = "yess"
+
+        print(context)
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    template_name = "books/book_detail.html"
+    context_object_name = "book"
+
+
+class ListAuthorView(generic.ListView):
+
+    model = Author
+    template_name = "authors/author_list.html"
+    context_object_name = "authors"
+
+    paginate_by = 3
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = "authors/author_detail.html"
+    context_object_name = "author"
